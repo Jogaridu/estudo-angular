885240dde44e601819498578fbb9cfaa311aaa96
Jorge Gabriel <e-jorge2010@hotmail.com>
2021-01-03 22:53:41 -0300

    Aprendizado em Angular

---
+++ front-end/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  // templateUrl aponta para o arquivo HTML desse componente
+  templateUrl: "app.component.html"
+})
+export class AppComponent {
+}
